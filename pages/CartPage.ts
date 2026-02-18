188ac952d64fa3383ed28ea18f2a79b738d9037a
mathew-agius <mathew.agius@dyedurham.com>
2026-02-19 09:40:21 +1100

    Update CartPage.ts
    dummy commit to trigger ci/cd

--- pages/CartPage.ts
+++ pages/CartPage.ts
@@ -4,7 +4,7 @@ export class CartPage {
   constructor(private page: Page) {}
 
   async assertProductInCart(productName: string) {
-    const item = this.page.locator('.cart_item').filter({ hasText: productName });
+    const item = this.page.locator('.cart_item').filter({ hasText: productName});
     await expect(item).toBeVisible();
   }
 
